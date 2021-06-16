430d7e1bc24b462b3119a293497a41fa0d1c3c82
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-06-17 00:03:10 +0300

    create new comment

--- routers/controllers/comments.js
+++ routers/controllers/comments.js
@@ -1,15 +1,16 @@
-const { db } = require('./../../db/models/comments');
-const commentsModel = require('./../../db/models/comments');
+// const { db } = require('./../../db/models/comments');
+// const commentsModel = require('./../../db/models/comments');
+const db = require("./../../db/db");
 
 const createNewComment = (req, res) => {
-	// const {comment,article_id,commenter_id} =req.body
-	// const query=`INSERT INTO comments (comment,article_id,commenter_id) 
-	// VALUES (?, ?, ?)`
-	// const arr =[comment,article_id,commenter_id];
-	// db.query(query,arr,(err,result)=>{
-	// 	if(err)throw err;
-	// 	res.json(result)
-	// })
+	const {comment,article_id,commenter_id} =req.body
+	const query=`INSERT INTO comments (comment,article_id,commenter_id) 
+	VALUES (?, ?, ?)`
+	const arr =[comment,article_id,commenter_id];
+	db.query(query,arr,(err,result)=>{
+		if(err)throw err;
+		res.json(result)
+	})
 
 };
 
